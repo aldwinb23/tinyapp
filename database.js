e32d84dccc551e0bfc1a10db37e91e8847de438f
aldwinb <aldwinb23@gmail.com>
2022-10-12 12:58:21 -0700

    Add: add new database.js and move database objects

---
+++ database.js
@@ -0,0 +1,27 @@
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "123",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+module.exports = { urlDatabase, users };
+
